74f1a1d17a45d174a6752e2c073b7cc123d42446
Mario Vinicius <marioviniciuxcs@gmail.com>
2024-11-03 22:10:14 -0300

    quase terminando

---
+++ src/js/script.js
@@ -0,0 +1,97 @@
+const modal = document.querySelector('.modal-container')
+const tbody = document.querySelector('tbody')
+const mProduto = document.getElementById('m-produto')
+const mQuantidade = document.getElementById('m-quantidade')
+const mValor = document.getElementById('m-valor')
+const btnSalvar = document.getElementById('btnSalvar')
+const btnNew = document.getElementById("new")
+
+let itens
+let id
+
+function openModal(edit = false, index = 0) {
+    modal.classList.add("active")
+    
+    modal.onclick = e => {
+        if (e.target.className.indexOf("modal-container") !== -1) {
+            modal.classList.remove("active")
+        }
+    }
+
+    if (edit) {
+        mProduto.value = itens[index].produto
+        mQuantidade.value = itens[index].quantidade
+        mValor.value = itens[index].valor
+        id = index 
+    } else {
+        mProduto.value = ''
+        mQuantidade.value = ''
+        mValor.value = ''
+    }
+}
+
+function editItem(index) {
+    openModal(true, index)
+}
+
+function deleteItem(index) {
+    itens.splice(index, 1)
+    setItensBD()
+    loadItens()
+}
+
+function insertItem(item, index) {
+    let tr = document.createElement('tr')
+
+    tr.innerHTML = `
+    <td>${item.produto}</td>
+    <td>${item.quantidade}</td>
+    <td>R$${(item.valor)}</td>
+    <td>R$ ${(item.quantidade * item.valor)}
+    <td class="acao">
+        <button onclick="editItem(${index})"><i class="bx bx-edit"></i></button>
+    </td>
+    <td class="acao">
+        <button onclick="deleteItem(${index})"><i class="bx bx-trash"></i></button>
+    </td>
+    `
+
+    tbody.appendChild(tr)
+}
+
+btnSalvar.addEventListener("click", e => {
+    if (mProduto.value == '' || mQuantidade.value == "" || mValor.value == '') {
+        return
+    }
+
+    e.preventDefault()
+
+    if (id !== undefined) {
+        itens[id].produto = mProduto.value
+        itens[id].quantidade = mQuantidade.value
+        itens[id].valor = mValor.value
+    } else {
+        itens.push({"produto": mProduto.value, "quantidade": mQuantidade.value, "valor": mValor.value})
+    }
+
+    setItensBD()
+
+    modal.classList.remove("active")
+    loadItens()
+    id = undefined
+})
+
+btnNew.addEventListener("click", openModal)
+
+function loadItens() {
+    itens = getItensBD()
+    tbody.innerHTML = ''
+    itens.forEach((item, index) => {
+        insertItem(item, index)
+    })
+}
+
+const getItensBD = () => JSON.parse(localStorage.getItem('dbfunc')) ?? []
+const setItensBD = () => localStorage.setItem("dbfunc", JSON.stringify(itens))
+
+loadItens()
--- src/kanbam.md
+++ src/kanbam.md
@@ -1,14 +1,12 @@
 TODO 
-- Lógica no JS
 - Testes
-- Tentar implementação do "banco de dados" com localStorage
-
-
+  
 DOING
-- Estilização no CSS, falta a estilização do modal e do botão
-
+- Falta somente fazer funcionar o cleaner de input quando clica em adicionar um outro produto, após já ter adicionado um 
 
 DONE
 - Organização das pastas e arquivos
 - Corpo do HTML
-  
+- Estilização no CSS, falta a estilização do modal e do botão
+- Lógica do modal, adição e remoção do conteúdo de forma dinâmica 
+- - Tentar implementação do "banco de dados" com localStorage
